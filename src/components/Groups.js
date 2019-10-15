624f8dd18162d9c6e8708d41a8e4a0b03b332a7b
cvanpoelje <47214924+cvanpoelje@users.noreply.github.com>
2019-10-15 13:01:34 +0200

    menu created

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -1,19 +1,7 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-
-import {Button, IconButton, AppBar, Toolbar, LinearProgress} from '@material-ui/core/';
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Pagination from "material-ui-flat-pagination";
-import {Badge} from 'reactstrap';
-import {isIOS, isMobile} from 'react-device-detect';
-
-
-const setDefaultState = () => {
-	return {
-		data: []
-    }
-}
-
+import React, { Component, useState } from "react";
+import Card from '@material-ui/core/Card';
+import { connect } from "react-redux";
+import { isMobile } from "react-device-detect";
 
 /***
  * Start Code Sectie
@@ -21,89 +9,130 @@ const setDefaultState = () => {
  * Material-UI 3.8.1
  * Bootstrap 4
  */
-
 /***
  * Jouw component styles
  */
 
+const styles = {
+  container:{
+    overflow: 'scroll'
+  },
+  listItem: {
+    padding: '10px',
+    margin: '5px',
+    cursor: 'pointer'
+  },
+  active: {
+    backgroundColor: 'rgba(0,0,0,.2)'
+  },
+  hover: {
+    backgroundColor: 'rgba(43,200,100,.2)'
+  }
+};
 
- /***
-  * Presentation Components
-  */
- const Presentation = props => {
- 	return (<div>Mijn presentatie code</div>);
- }
+/***
+ * Presentation Components
+ */
 
+const Presentation = props => {
+  const [mouseInside, setMouseInside] = useState();
+  const [showWidget, setShowWidget] = useState();
+  const clickHandler = () => {
+    setShowWidget(!showWidget);
+  };
+
+  return (
+    <Card style={  showWidget?styles.active:null, mouseInside?styles.hover:null ,styles.listItem}
+        onClick= {() => clickHandler()}
+        onMouseEnter={()=>{setMouseInside(true)}}
+        onMouseLeave={()=>{setMouseInside(false)}}
+      >
+    <h1>{props.group.grp_name}</h1>
+    <p>{props.group.grp_teaser}</p>
+      {showWidget ? (
+        <div>
+          <p>{props.group.founder.fullName} is the founder.</p>
+          List of Owners :
+          <ul>
+            {props.group.owners.map( 
+              owner => <li>{owner.fullName} is an owner</li>)}
+          </ul>
+        </div>
+      ) : null}
+      
+    </Card>
+
+  );
+};
 
 /***
  * Jouw Component Code
  */
 class Groups extends Component {
+  renderMobile(groupList) {
+    return (
+      <div>
+        {groupList.map(group => (
+          <Presentation key={group.id} group={group} />
+        ))}
+      </div>
+    );
+  }
 
+  renderDesktop(groupList) {
+    return (
+      <div>
+        {groupList.map(group => (
+          <Presentation key={group.id} group={group}/>
+        ))}
+      </div>
+    );
+  }
 
-	renderMobile(groupList) {
+  render() {
+    // get list of groups
+    const { client, group } = this.props;
 
-		// const {orientation, viewPort, device, scroll } = this.props.client.screen;
+    /***
+     * Display Settings
+     * widths, heights, orientation (portrait/landscape)
+     * Display orientation change is detected
+     */
 
-		return (<div>
-			<h1>Lijst</h1>
-			{groupList.map( group => <Presentation key={group.id}/>)}
-		</div>)
+    const { screen } = client;
+    if (!screen) return null;
 
-	}
+    const {
+      orientation, // portrait-primary, portrait=landscape
+      viewPort, // clientWIdth, offsetWidth, scrollWidth etc.
+      device, // widht, availableWidth, height, availableHeight
+      scroll // clientWIdth, offsetWidth, scrollWidth, scrollHeight etc.
+    } = screen;
 
-	renderDesktop(groupList) {
-		// const {orientation, viewPort, device, scroll } = this.props.client.screen;
-		return(<div>
-			<h1>Lijst</h1>
-			{groupList.map( group => <Presentation key={group.id}/>)}
-		</div>);
-	}
+    /***
+     * Mock data
+     */
+    const groupList = group.data;
 
-	render() {
-
-		// get list of groups
-		const {client, group} = this.props;		
-
-		console.log(client);
-
-		/***
-		 * Display Settings
-		 * widths, heights, orientation (portrait/landscape)
-		 * Display orientation change is detected
-		 */     
-		const {screen} = client;
-		if(!screen) return null;
-
-		const {
-			orientation, // portrait-primary, portrait=landscape
-			viewPort, // clientWIdth, offsetWidth, scrollWidth etc.
-			device, // widht, availableWidth, height, availableHeight
-			scroll // clientWIdth, offsetWidth, scrollWidth, scrollHeight etc.
-		} = screen;
-
-		/***
-		 * Mock data
-		 */
-		const groupList = group.data;		
- 
-		return (
-			<div className="container">
-				{isMobile ? this.renderMobile(groupList) : this.renderDesktop(groupList) }				
-			</div>
-		);
-	}
+    return (
+      <div className="container" >
+        {isMobile
+          ? this.renderMobile(groupList)
+          : this.renderDesktop(groupList)}
+      </div>
+    );
+  }
 }
-
 
 /***
  * Einde Code Sectie
  */
 
-
-const mapStateToProps = ({client, group}) => {		
-	return {client, group};
+const mapStateToProps = ({ client, group }) => {
+  return { client, group };
 };
 
-export default connect(mapStateToProps, {	
-})(Groups);
+export default connect(
+  mapStateToProps,
+  {}
+)(Groups);
